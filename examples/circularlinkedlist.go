6e07a1827075d255f2283ac931dc0667a5ce6d68
Matheus Cunha <matheus.cunha@g.globo>
2024-05-30 16:56:21 -0300

    add circular linked list example

---
+++ examples/circularlinkedlist.go
@@ -0,0 +1,39 @@
+package examples
+
+import (
+	"fmt"
+	"gotas/lists/circularlinkedlist"
+)
+
+func CircularLinkedListExample() {
+	list := circularlinkedlist.New[int]()
+
+	fmt.Println("Appending values 1, 2 and 3...")
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	fmt.Printf("%v", list.Values())
+
+	fmt.Println("\n\nPrepending value 0...")
+	list.Prepend(0)
+	fmt.Println(list.Values())
+
+	fmt.Println("\nAdding value 4 at index 2...")
+	list.Add(2, 4)
+	fmt.Println(list.Values())
+
+	fmt.Println("\nDeleting element at index 3...")
+	list.Delete(3)
+	fmt.Println(list.Values())
+
+	fmt.Println("\nDeleting first element...")
+	list.Delete(0)
+	fmt.Println(list.Values())
+
+	fmt.Println("\nDeleting last element...")
+	list.Delete(2)
+	fmt.Println(list.Values())
+
+	fmt.Println("\nChecking if the list is empty...")
+	fmt.Println(list.Empty())
+}
--- main.go
+++ main.go
@@ -8,7 +8,13 @@ import (
 func main() {
 	fmt.Printf("==== Singly Linked List ===\n")
 	examples.SinglyLinkedListExample()
+	fmt.Printf("\n=========================\n")
 
 	fmt.Printf("\n==== Doubly Linked List ====\n")
 	examples.DoublyLinkedListExample()
+	fmt.Printf("\n============================\n")
+
+	fmt.Printf("\n==== Circular Linked List ====\n")
+	examples.CircularLinkedListExample()
+	fmt.Printf("\n==============================\n")
 }
