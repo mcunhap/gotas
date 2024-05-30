e6f2254861c2a1f5fff5796469a01caf3a127428
Matheus Cunha <matheus.cunha@g.globo>
2024-05-30 15:09:16 -0300

    add doubly linked list example

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "gotas/examples"
+import (
+	"fmt"
+	"gotas/examples"
+)
 
 func main() {
+	fmt.Printf("==== Singly Linked List ===\n")
 	examples.SinglyLinkedListExample()
+
+	fmt.Printf("\n==== Doubly Linked List ====\n")
+	examples.DoublyLinkedListExample()
 }
